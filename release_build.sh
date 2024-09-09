f088cc7778bd8635ec25a0ef668732542980eaa6
Jonathan Kagan <jon@lul.dev>
2024-09-09 17:30:22 -0400

    Temp remove

--- release_build.sh
+++ release_build.sh
@@ -43,20 +43,6 @@ for r in "${KBS_VERS_ARRAY[@]}"; do
     CRIO+=( "crio-${version:1}" )
 done
 
-echo
-echo "Fetching previous 'latest' release sysexts"
-echo "=========================================="
-curl -fsSL --retry-delay 1 --retry 60 --retry-connrefused \
-         --retry-max-time 60 --connect-timeout 20  \
-         https://api.github.com/repos/darkspadez/sysext-bakery/releases/latest \
-    | jq -r '.assets[] | "\(.name)\t\(.browser_download_url)"' | { grep -E '\.raw$' || true; } | tee prev_release_sysexts.txt
-
-while IFS=$'\t' read -r name url; do
-    echo
-    echo "  ## Fetching ${name} <-- ${url}"
-    curl -o "${name}" -fsSL --retry-delay 1 --retry 60 --retry-connrefused --retry-max-time 60 --connect-timeout 20  "${url}"
-done <prev_release_sysexts.txt
-
 streams=()
 
 echo
